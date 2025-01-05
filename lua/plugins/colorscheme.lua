5e4b7b1d05baef1832838bb4751ecf59a70469d9
Kartikay Dubey <kartikay21326@iiitd.ac.in>
2025-01-05 03:32:12 -0800

    tmux-no-tabs

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,22 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = { style = "night", transparent = true },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night", transparent = true },
+  },
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "dark",
+      transparent = true,
+    },
+  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = function()
+  --       require("onedark").load()
+  --     end,
+  --   },
+  -- },
 }
